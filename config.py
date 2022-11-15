467a23cfc3d0e19abcea2110add1af08a10c39fa
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-11-15 21:42:41 +0300

    Add config

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
-    'Accept': 'text/html, */*; q=0.01',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'X-Requested-With': 'XMLHttpRequest',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://fipi.ru/ege/otkrytyy-bank-zadaniy-ege',
     'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
+    'Upgrade-Insecure-Requests': '1',
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache'
 }
