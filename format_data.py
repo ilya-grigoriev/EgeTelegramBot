cda7c8abfcbe5df47a13c0afc2f84bacc9a63196
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-11-17 22:53:17 +0300

    Add functions for formatting

---
+++ format_data.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+
+def format_choosing_answers(answers: list[str]) -> list[str]:
+    formatted_answers = []
+    for count, answer in enumerate(answers, start=1):
+        formatted_answers.append(f'{count}) {answer}')
+
+
+def format_desc_for_task(text: str) -> dict:
+    data = dict()
+    soup = BeautifulSoup(text, 'html.parser')
+    text_task = soup.find('div', attrs={'name': 'text'}).text
+    data['text_task'] = text_task.strip('\n')
+    answers = [answer.text.strip() for answer in
+               soup.find_all('div', attrs={'class': 'answer'})]
+    data['answers'] = answers
+    return data
+
+
+def format_tasks(tasks: list[dict]) -> list[list[str, dict]]:
+    data = []
+    for task in tasks:
+        level_name = task.get('levelName', '').strip()
+        task_title = task.get('taskTitle', '').strip()
+        ind_answer = task.get('answer', '').strip()
+        desc_for_task = format_desc_for_task(task.get('html', '').strip())
+        data.append([level_name, task_title, desc_for_task, ind_answer])
+    return data
