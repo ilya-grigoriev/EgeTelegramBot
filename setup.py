31f14eae4604925ca0cdd0497e826918412d3d36
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-12-11 22:38:24 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EgeTelegramBot',
+    author='Ilya Grigoryev',
+    version='0.1',
+    package_data={
+        'parse_data': ['py.typed'],
+        'handlers': ['py.typed']
+    },
+    packages=['parse_data', 'handlers']
+)
