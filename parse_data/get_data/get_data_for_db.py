67fc06bfd74fc3ce2ba53458c36ab140f7280322
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-12-16 21:04:15 +0300

    Add pydantic validator

--- parse_data/get_data/get_data_for_db.py
+++ parse_data/get_data/get_data_for_db.py
@@ -1,26 +1,14 @@
-from parse_data.format.format_data_in_tag import delete_excess_data_in_tag
-from parse_data.typing_for_parsing import DataForDB
+from parse_data.typing_for_parsing import DataForDB, Task, data_from_json
 
 
-def get_data_from_json(*, task: dict) -> DataForDB | None:
-    if task.get('answer') and task.get('id'):
-        id = task.get('id', -1)
-
-        level_name = task.get('levelName', '').strip()
-
-        number_task = task.get('numberInGroup', '')
-        number_task = number_task if number_task else -1
-
-        correct_answer = task.get('answer', '')
-
-        html_code = task.get('html', '')
-        html_code = html_code.strip().replace('\n', ' ').replace('\r', ' ')
-
-        html_code = delete_excess_data_in_tag(html_code)
-
-        task_data = DataForDB(level_name=level_name, html=html_code,
-                              number_task=number_task, id_task=id,
-                              correct_answer=correct_answer)
+def get_data_from_json(*, task: data_from_json) -> DataForDB | None:
+    formatted_task = Task(**task)
+    if formatted_task.answer and formatted_task.id:
+        task_data = DataForDB(level_name=formatted_task.level_name,
+                              html=formatted_task.html,
+                              number_task=formatted_task.number_task,
+                              id_task=formatted_task.id,
+                              correct_answer=formatted_task.answer)
 
         return task_data
     return None
