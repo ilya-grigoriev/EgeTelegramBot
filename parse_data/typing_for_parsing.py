1cb370d9a8714b5548578df462b9eee176eca608
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-12-16 21:04:06 +0300

    Add pydantic class

--- parse_data/typing_for_parsing.py
+++ parse_data/typing_for_parsing.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
-from typing import TypeVar
+from pydantic import BaseModel, Field, validator
+
+from parse_data.format.format_data_in_tag import delete_excess_data_in_tag
 
 id_task_from_db = int
+data_from_json = dict
 
 
 @dataclass
@@ -11,3 +14,26 @@ class DataForDB:
     number_task: int = -1
     html: str = ''
     correct_answer: str = ''
+
+
+class Task(BaseModel):
+    id: int | None = Field(default=-1)
+    level_name: str = Field(alias='levelName')
+    number_task: int | None = Field(default=-1, alias='numberInGroup')
+    answer: str
+    html: str
+
+    @validator('level_name', 'answer')
+    def check_truth_str_obj(cls, val):
+        return val.strip() if val else ''
+
+    @validator('html')
+    def check_and_format_html(cls, html):
+        html_code = html.strip().replace('\n', ' ').replace('\r', ' ')
+
+        html_code = delete_excess_data_in_tag(html_code)
+        return html_code
+
+    @validator('id', 'number_task')
+    def check_truth_int_obj(cls, val):
+        return val if val else -1
