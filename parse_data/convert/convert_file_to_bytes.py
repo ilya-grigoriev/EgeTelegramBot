bb0f9edfd1ecfadf4bbe1e30c5e2b375848d9072
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2022-12-18 21:54:58 +0300

    Add function convert image to bytes

--- parse_data/convert/convert_file_to_bytes.py
+++ parse_data/convert/convert_file_to_bytes.py
@@ -1,21 +1,10 @@
-import io
-from PIL import Image
-
-
-def convert_image_to_bytes(*, file_name: str) -> io.BytesIO:
-    # converted_image = io.BytesIO()
-    #
-    # image = Image.open(file_name)
-    # image = image.convert('RGBA')
-    # #
-    # image.save(converted_image, "PNG")
-    # converted_image.seek(0)
-    # converted_image = io.BytesIO(converted_image.read())
-    img = Image.open(file_name)
-    bytes_io = io.BytesIO()
-    img.save(bytes_io, format='PNG')
-    converted_image = bytes_io.getvalue()
-    return io.BytesIO(converted_image)
+def convert_image_to_bytes(*, file_name: str) -> bytes:
+    converted_image = None
+    with open(file_name, "rb") as image:
+        to_bytes = image.read()
+        converted_image = to_bytes
+    image.close()
+    return converted_image
 
 
 if __name__ == '__main__':
