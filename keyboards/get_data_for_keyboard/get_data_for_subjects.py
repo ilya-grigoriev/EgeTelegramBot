57888818fbfaf75542b784f7e238a5706f72815c
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-01-06 17:14:15 +0300

    Refactor code

---
+++ keyboards/get_data_for_keyboard/get_data_for_subjects.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from parse_data.config_for_parsing import subjects_en
+from parse_data.get_data.get_data_of_subject import get_json_of_data_subject
+from parse_data.typing_for_parsing import DataTask, DataSubjectForTG
+
+
+def get_subjects_data() -> List[DataSubjectForTG]:
+    data_for_tg = []
+    for subject in subjects_en:
+        issues = get_json_of_data_subject(
+            subject_name_en=subject)
+        converted_data = [DataTask(**issue) for issue in issues]
+        formatted_data = [issue for issue in converted_data if
+                          issue.subtopics and issue.issue != 0]
+
+        subject_dict = {'title': subject, 'issues': formatted_data}
+        data_for_tg.append(DataSubjectForTG(**subject_dict))
+    return data_for_tg
+
+
+if __name__ == '__main__':
+    print(get_subjects_data())
