94d6ac5ec1891b5d7979526014ba824b1b2a33ba
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-01-06 17:14:33 +0300

    Add initializing file for bot

---
+++ bot_for_tg/init_for_bot.py
@@ -0,0 +1,16 @@
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from work_with_db.check_data.check_existing_db import check_db
+from keyboards.get_data_for_keyboard.get_data_for_subjects import get_subjects_data
+
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+bot = Bot(token=TELEGRAM_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+# check_db()
+scheduler = AsyncIOScheduler()
+subjects_data_for_keyboard = get_subjects_data()
