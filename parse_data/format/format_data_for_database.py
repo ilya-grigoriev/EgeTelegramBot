e0f4160d43ebecf5d82c1df29080a13a14cca607
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-01-07 04:12:41 +0300

    Refactor code

--- parse_data/format/format_data_for_database.py
+++ parse_data/format/format_data_for_database.py
@@ -12,6 +12,6 @@ def format_data_for_db(*, task: DataTaskOfSubtopic, number_task: int,
     if number_subtopic != -1:
         task_section = f'{number_task}/{number_subtopic}'
     else:
-        task_section = number_task
+        task_section = f'{number_task}/0'
     total_request = f"('{task_section}', {task.id_task}, {str(is_detailed).lower()}, '{task.task_desc_html}', '{task.text_for_task_html}', '{task.solution_html}', '{task.answer}')"
     return total_request
