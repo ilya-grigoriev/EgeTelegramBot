ffc9587c6d11de0697f83d15cd735c8b551158d0
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-01-07 04:17:27 +0300

    Created module for common function

---
+++ handlers/handlers_for_task.py
@@ -0,0 +1,17 @@
+from aiogram import types, Bot
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardRemove
+
+from handlers.get_data import get_random_task
+from keyboards.menu import keyboard_menu
+
+
+async def send_task(*, message: types.Message, state: FSMContext, bot: Bot):
+    await state.update_data({'is_sending': True})
+    await message.answer(
+        "Идёт отправка задания...", reply_markup=ReplyKeyboardRemove()
+    )
+
+    await get_random_task(message=message, state=state, bot=bot)
+    await message.answer('Выберите действие:', reply_markup=keyboard_menu)
+    await state.update_data({'is_sending': False})
