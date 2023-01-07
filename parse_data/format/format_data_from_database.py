7362d3a6e6395edd43b5800b900c4df3e9617d51
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-01-08 00:36:53 +0300

    Added formatting for html codes

--- parse_data/format/format_data_from_database.py
+++ parse_data/format/format_data_from_database.py
@@ -1,14 +1,19 @@
-from typing import Tuple, Optional
 from parse_data.convert.convert_html import convert_html_code_to_image
-from parse_data.convert.convert_file_to_bytes import convert_image_to_bytes
-from parse_data.typing_for_parsing import DataFromDB, DataForTG
+from parse_data.format.format_data_in_tag import delete_excess_data_in_tag
+from parse_data.typing_for_parsing import DataFromDB
 from aiogram import Bot
 
 
 async def format_data_for_tg_and_send_photo(*, data: DataFromDB, bot: Bot,
-                                            chat_id: str):
+                                            chat_id: str,
+                                            subject_name_en: str):
+    template_url = f'https://{subject_name_en}-ege.sdamgia.ru'
+
+    formatted_task_desc_html = delete_excess_data_in_tag(
+        template_url=template_url, tag=data.task_desc_html)
+
     task_images = await convert_html_code_to_image(
-        html_code=data.task_desc_html,
+        html_code=formatted_task_desc_html,
         type_html='task',
         id_task=data.id_task)
 
@@ -17,8 +22,12 @@ async def format_data_for_tg_and_send_photo(*, data: DataFromDB, bot: Bot,
         await bot.send_photo(chat_id=chat_id, photo=task_image)
 
     if data.text_for_task_html:
+        formatted_text_for_task_html = delete_excess_data_in_tag(
+            template_url=template_url, tag=data.text_for_task_html)
+
         text_for_task_images = await convert_html_code_to_image(
-            html_code=data.text_for_task_html, type_html='text_for_task',
+            html_code=formatted_text_for_task_html,
+            type_html='text_for_task',
             id_task=data.id_task)
 
         await bot.send_message(chat_id=chat_id, text='Текст задания:')
@@ -36,8 +45,11 @@ async def format_data_for_tg_and_send_photo(*, data: DataFromDB, bot: Bot,
         for answer_image in answer_images:
             await bot.send_photo(chat_id=chat_id, photo=answer_image)
     else:
+        formatted_solution_html = delete_excess_data_in_tag(
+            template_url=template_url, tag=data.solution_html)
+
         solution_images = await convert_html_code_to_image(
-            html_code=data.solution_html,
+            html_code=formatted_solution_html,
             type_html='solution',
             id_task=data.id_task)
 
