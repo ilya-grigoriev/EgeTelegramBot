0a4a5018e57774801a0cbf9a98219121ad53d682
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-02-02 22:11:17 +0300

    fix: remove opening file

--- logger_for_project.py
+++ logger_for_project.py
@@ -1,15 +1,7 @@
 """This module is designed for creating logger."""
-import os.path
-
 import loguru
 from parse_data.config_for_parsing import PATH_DIR
 
 file_path = f"{PATH_DIR}\\out.log"
-if not os.path.isfile(file_path):
-    with open(file_path, mode="w", encoding="utf-8") as file:
-        file_out = file
-else:
-    with open(file_path, mode="a", encoding="utf-8") as file:
-        file_out = file
 my_logger = loguru.logger
-my_logger.add(file_out)
+my_logger.add(file_path)
