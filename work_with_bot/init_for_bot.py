8e51a27bcac48bd2f2d08eeaac7f300b2eeb36f2
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-02-02 21:42:37 +0300

    feat: add loading dotenv

--- work_with_bot/init_for_bot.py
+++ work_with_bot/init_for_bot.py
@@ -1,10 +1,16 @@
+"""This module is designed for initializing Telegram bot."""
 import os
+from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from keyboards.get_data_for_keyboard.get_data_for_subjects import get_subjects_data
+from keyboards.get_data_for_keyboard.get_data_for_telegram_subjects import (
+    get_subjects_data,
+)
+
+load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TELEGRAM_TOKEN)
@@ -16,6 +22,8 @@ subjects_data_for_keyboard = get_subjects_data()
 
 
 class Response(StatesGroup):
+    """Aiogram State for user interaction."""
+
     issue = State()
     subtopic = State()
     task = State()
