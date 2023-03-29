a326b51eae5319105f99af41c026ca1536fb22d5
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-29 17:54:14 +0300

    feat: updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 # flake8: noqa
-from distutils.core import setup
+from setuptools import find_packages, setup
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+with open("LICENSE", encoding="utf-8") as f:
+    license_text = f.read()
 
 setup(
     name="EgeTelegramBot",
+    version="0.2.4",
+    description="Telegram bot created to solve the tasks of the USE",
+    long_description=readme,
     author="Ilya Grigoryev",
-    version="0.1",
-    package_data={"parse_data": ["parse_data"], "handlers": ["py.typed"]},
-    packages=["parse_data", "handlers"],
+    author_email="ilyagrigoryevworkmail@gmail.com",
+    url="https://github.com/Ilya-Grigoriev/EgeTelegramBot",
+    license=license_text,
+    packages=find_packages(exclude=("tests")),
 )
