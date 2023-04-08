f470f0d459913afdd19333d87f76774547a262a1
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-04-08 21:57:02 +0300

    fix(parse_data/format/tests): added condition for testing

--- parse_data/format/tests/test_format_data_for_database.py
+++ parse_data/format/tests/test_format_data_for_database.py
@@ -1,12 +1,14 @@
-from hypothesis import given, strategies as st
 import pytest
-
+from hypothesis import given
+from hypothesis import strategies as st
 from parse_data.format.format_data_for_database import format_data_for_db
 from parse_data.typing_for_parsing import DataTaskOfSubtopic
 
 
 @given(
-    number_task=st.integers(), is_detailed=st.booleans(), number_subtopic=st.integers()
+    number_task=st.integers(),
+    is_detailed=st.booleans(),
+    number_subtopic=st.integers(),
 )
 def test_incorrect_data_for_format_data_for_db(
     number_task, is_detailed, number_subtopic
@@ -19,10 +21,11 @@ def test_incorrect_data_for_format_data_for_db(
         solution_html="",
         answer="",
     )
-    with pytest.raises():
-        format_data_for_db(
-            task=task,
-            number_task=number_task,
-            is_detailed=is_detailed,
-            number_subtopic=number_subtopic,
-        )
+    if number_task <= 0 or number_subtopic <= 0:
+        with pytest.raises(Exception):
+            format_data_for_db(
+                task=task,
+                number_task=number_task,
+                is_detailed=is_detailed,
+                number_subtopic=number_subtopic,
+            )
